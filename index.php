ef2ed51e0780b45fe5b7a5aac48abf2b3344e732
0Zetrium0 <lolchen1510@gmail.com>
2025-12-17 12:53:26 +0100

    Ajout de tout les fichiers

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Inventaire de Fruits</title>
+</head>
+<body>
+    <h1>Inventaire de Fruits</h1>
+
+    <div class="fruit" data-quantity="10">
+        <img src="images/pomme.jpg" alt="Pomme">
+        <div>
+            Quantité disponible : <span class="quantity">10</span>
+        </div>
+	<button>+</button>
+	<button>-</button>
+    </div>
+</body>
+</html>
+
